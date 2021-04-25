74415da09f70227729a12bff5e4cce35cee71084
Meer Sawood <msawood@redhat.com>
2021-04-25 13:25:04 +0530

    added color

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -13,18 +13,15 @@ class MyHomePage extends StatelessWidget {
   static const title = "Noor-e-Mehdavia";
 
   get page => null;
-   
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-       appBar: AppBar(
+      appBar: AppBar(
         automaticallyImplyLeading: true,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(30),
             side: BorderSide(width: 2, color: Colors.white)),
-      
         titleSpacing: 7.0,
         title: Container(
           child: Text(
@@ -41,80 +38,45 @@ class MyHomePage extends StatelessWidget {
             systemNavigationBarIconBrightness: Brightness.dark),
       ),
       extendBodyBehindAppBar: false,
-     /*  body: Center(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text('${context.watch<Counter>().getCounter}')]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        key: const Key('increment_floatingActionButton'),
-
-        /// Calls `context.read` instead of `context.watch` so that it does not rebuild
-        /// when [Counter] changes.
-        onPressed: () =>
-            Provider.of<Counter>(context, listen: false).incrementCounter(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), */
-
       bottomNavigationBar: PandaBar(
+        backgroundColor: Theme.of(context).primaryColor,
         buttonData: [
+          PandaBarButtonData(id: 'Grey', icon: Icons.dashboard, title: 'Grey'),
+          PandaBarButtonData(id: 'Blue', icon: Icons.book, title: 'Blue'),
           PandaBarButtonData(
-            id: 'Grey',
-            icon: Icons.dashboard,
-            title: 'Grey'
-          ),
+              id: 'Red', icon: Icons.account_balance_wallet, title: 'Red'),
           PandaBarButtonData(
-            id: 'Blue',
-            icon: Icons.book,
-            title: 'Blue'
-          ),
-          PandaBarButtonData(
-            id: 'Red',
-            icon: Icons.account_balance_wallet,
-            title: 'Red'
-          ),
-          PandaBarButtonData(
-            id: 'Yellow',
-            icon: Icons.notifications,
-            title: 'Yellow'
-          ),
+              id: 'Yellow', icon: Icons.notifications, title: 'Yellow'),
         ],
         onChange: (id) {
           setState(() {
-                     var page = id;
-                    });
-                  },
-                  onFabButtonPressed: () {
-          
-                  },
-                ),
-                body: Builder(
-                  builder: (context) {
-          
-                    switch (page) {
-                      case 'Grey':
-                        return Container(color: Colors.grey.shade900);
-                      case 'Blue':
-                        return Container(color: Colors.blue.shade900);
-                      case 'Red':
-                        return Container(color: Colors.red.shade900);
-                      case 'Yellow':
-                        return Container(color: Colors.yellow.shade700);
-                      default:
-                        return Container();
-                        
-                    }
-          
-                  },
-                ),
-              
-              );
-            }
+            var page = id;
+          });
+        },
+        onFabButtonPressed: () {
+          return Container(
+            child: Text("Hello"),
+          );
+        },
+      ),
+      body: Builder(
+        builder: (context) {
+          switch (page) {
+            case 'Grey':
+              return Container(color: Colors.black);
+            case 'Blue':
+              return Container(color: Colors.blue.shade900);
+            case 'Red':
+              return Container(color: Colors.red.shade900);
+            case 'Yellow':
+              return Container(color: Colors.yellow.shade700);
+            default:
+              return Container();
           }
-          
-          void setState(Null Function() param0) {
+        },
+      ),
+    );
+  }
 }
 
-
+void setState(Null Function() param0) {}
